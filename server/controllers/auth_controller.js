8e0aae1dabdd84f022087807cc6719f3a2abc840
Beth Telford <mrsbethtelford@gmail.com>
2018-09-02 17:43:46 -0600

    Basic endpoint setup

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -5,5 +5,12 @@ module.exports = {
     const user = await db.register({name, username, email, password});
     console.log(user)
     res.status(200).send(user[0])
+  },
+  login: async (req, res) => {
+    const db = req.app.get('db');
+    const { username, password } = req.body;
+    const user = await db.login({username, password});
+    console.log(user);
+    res.status(200).send(user[0])
   }
 }
