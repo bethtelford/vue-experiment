874dccc368cd8063079e18404a30e2ece05cc40c
Beth Telford <mrsbethtelford@gmail.com>
2018-09-02 16:23:28 -0600

    Basic register form handlers in place

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="register">
-    <form>
+    <form @submit="complete">
       Name: <input v-model="name"/>
       Username: <input v-model="username"/>
       Email: <input 
         v-model="email" 
+        @blur='validateEmail'
       />
       Password: <input 
         v-model="password" 
+        @blur="validatePasswordLength"
         type='password'
       />
       Confirm Password: <input 
         v-model="confirmPassword"
+        @blur="validatePasswordMatch"
         type='password'
       />
+      <button>Complete</button>
     </form>
+    <div v-show="showModule">
+      I am the module 
+      <button @click="register">Confirm</button>
+      <button @click="cancel">Cancel</button>
+    </div>
   </div>
 </template>
 
@@ -32,6 +41,39 @@ export default {
       showModule: false,
       allowSubmit: true
     };
+  },
+  methods: {
+    validateEmail() {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(String(this.email).toLowerCase())) {
+        console.log("you need a valid email address");
+        this.allowSubmit = false;
+      }
+    },
+    validatePasswordLength() {
+      if (this.password.length < 12) {
+        console.log("you need a longer password", this.password);
+        this.allowSubmit = false;
+      }
+    },
+    validatePasswordMatch() {
+      if (this.password !== this.confirmPassword) {
+        console.log("does not match");
+        this.allowSubmit = false;
+      }
+    },
+    complete(e) {
+      e.preventDefault();
+      if (this.allowSubmit) {
+        this.showModule = true;
+      }
+    },
+    register() {
+      console.log('axios request here')
+    },
+    cancel() {
+      this.showModule = false;
+    }
   }
 };
 </script>
