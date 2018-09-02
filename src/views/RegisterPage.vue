1d5c6357cd91d435fad5d5819a82dbfdd03f2da5
Beth Telford <mrsbethtelford@gmail.com>
2018-09-02 16:42:00 -0600

    better data validation with module

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="register">
-    <form @submit="complete">
+    <form @submit="toggleModule">
       Name: <input v-model="name"/>
       Username: <input v-model="username"/>
       Email: <input 
@@ -38,38 +38,57 @@ export default {
       email: "",
       password: "",
       confirmPassword: "",
+      // boolean for module conditional rendering
       showModule: false,
-      allowSubmit: true
+      // allowModule determines if the 'toggleModule' function is allowed to fire, based on the data validation happening in the form
+      allowModule: true
     };
   },
   methods: {
     validateEmail() {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (!re.test(String(this.email).toLowerCase())) {
+      if (re.test(String(this.email).toLowerCase())) {
+        this.allowModule = true;
+      } else {
         console.log("you need a valid email address");
-        this.allowSubmit = false;
+        this.allowModule = false;
       }
     },
     validatePasswordLength() {
-      if (this.password.length < 12) {
+      if (this.password.length >= 12) {
+        this.allowModule = true;
+      } else {
         console.log("you need a longer password", this.password);
-        this.allowSubmit = false;
+        this.allowModule = false;
       }
     },
     validatePasswordMatch() {
-      if (this.password !== this.confirmPassword) {
+      if (this.password === this.confirmPassword) {
+        this.allowModule = true;
+      } else {
         console.log("does not match");
-        this.allowSubmit = false;
+        this.allowModule = false;
       }
     },
-    complete(e) {
+    toggleModule(e) {
       e.preventDefault();
-      if (this.allowSubmit) {
+      // if allowModule is true, the data validation has been successful, and all fields must have values
+      if (
+        this.allowModule &&
+        this.name &&
+        this.username &&
+        this.email &&
+        this.password &&
+        this.confirmPassword
+      ) {
         this.showModule = true;
       }
+      // this.showModule = true;
     },
     register() {
-      console.log('axios request here')
+      console.log("axios request here");
+      // push method to fire after HTTP response
+      this.$router.push('login')
     },
     cancel() {
       this.showModule = false;
