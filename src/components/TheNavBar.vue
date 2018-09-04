c620636ff91dfd453cb8ded2f51b5f066c5587b2
Beth Telford <mrsbethtelford@gmail.com>
2018-09-03 22:11:18 -0600

    Styled navbar, cleaned up navbar methods, hid logout button when appropriate

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,43 +1,80 @@
 <template>
-  <div class='nav'>
-    <h1>{{title}}</h1>
-    <button 
-      to='/login'
-      @click="logout"
-    >Logout</button>
-  </div>
+  <v-stepper-header class='nav-bar'>
+    <v-layout 
+      align-center 
+      justify-center
+    >
+      <h1>{{title}}</h1>
+      <button 
+        class='logout links'
+        to='/login'
+        @click="logout"
+        v-show="showLogout"
+      >
+        Logout
+      </button>
+    </v-layout>
+  </v-stepper-header>
 </template>
 
 <script>
-import axios from 'axios';
-import { mapMutations } from 'vuex';
+import axios from "axios";
+import { mapMutations } from "vuex";
 export default {
   name: "TheNavBar",
   data() {
-    let title = this.$router.history.current.name;
-    title = title[0].toUpperCase() + title.slice(1);
     return {
-      title: title
+      title: this.changeTitle(),
+      showLogout: this.changeLogout()
     };
   },
   watch: {
     $route(to, from) {
-      let title = to.name;
-      title = title[0].toUpperCase() + title.slice(1);
-      this.title = title;
+      // Changing the name on display in the NavBar
+      this.title = this.changeTitle();
+      // Showing the logout button if relevant
+      this.showLogout = this.changeLogout();
     }
   },
   methods: {
-    ...mapMutations(['logoutUser']),
+    ...mapMutations(["logoutUser"]),
+    changeTitle() {
+      let title = this.$router.history.current.name;
+      return title[0].toUpperCase() + title.slice(1);
+    },
+    changeLogout() {
+      let title = this.$router.history.current.name;
+      if (title === "users") {
+        return true;
+      } else {
+        return false;
+      }
+    },
     async logout() {
       try {
-        await axios.post('/auth/logout');
+        await axios.post("/auth/logout");
         this.logoutUser();
-        this.$router.push('login');
+        this.$router.push("login");
       } catch (err) {
-        console.log('oops something went wrong');
+        console.log("oops something went wrong");
       }
     }
   }
 };
 </script>
+
+<style scoped>
+.nav-bar {
+  background-color: #c4c4c4;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+.logout {
+  position: absolute;
+  right: 15px;
+  top: 25px;
+}
+</style>
